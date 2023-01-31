6c629c5a1c02aa5565464eb7104c60e77c1dd74d
Ernesto Guaman <ernesto.guaman@enviame.io>
2023-01-30 20:23:49 -0500

    UT8-1 feat:update admin django models customers

--- django-backend/customers/admin.py
+++ django-backend/customers/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
-
+from customers.models import *
 # Register your models here.
+admin.site.register(Category)
+class CustumerAdmin(admin.ModelAdmin):
+    list_display = (
+        'code',
+        'identification',
+        'category',
+        'names',
+        'gender',
+        'email',
+        'status'
+    )
+    list_per_page = 20
+    search_fields = ('code','identification','email')
+    list_filter = (
+        'category',
+        'deleted',
+    )
+    def status(self, obj):
+        return not obj.deleted
+    status.boolean = True
+
+admin.site.register(Custumer, CustumerAdmin)
